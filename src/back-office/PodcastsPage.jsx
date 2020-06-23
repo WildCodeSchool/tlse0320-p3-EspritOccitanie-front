c64ff3c77dafb40a6f22b6fb5242626158c871af
tzoitzeot <tristan.maciag@icloud.com>
2020-06-23 07:55:06 +0200

    BACKOFFICE - create left navbar

---
+++ src/back-office/PodcastsPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const PodcastsPage = props => {
+  return <div></div>;
+};
+
+PodcastsPage.propTypes = {};
+
+export default PodcastsPage;
