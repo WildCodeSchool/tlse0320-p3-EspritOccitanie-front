fec65a41da9ebe43872ddb722c38b05d3d1848fb
tzoitzeot <tristan.maciag@icloud.com>
2020-06-30 14:51:31 +0200

    install router & separation admin / client with routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navigation from './back-office/Navigation';
+import Podcasts from './client/Podcasts';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Navigation />
+      <Router>
+        <Switch>
+          <Route path="/podcasts">
+            <Podcasts />
+          </Route>
+          <Route path="/admin-radio-occitanie">
+            <Navigation />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
