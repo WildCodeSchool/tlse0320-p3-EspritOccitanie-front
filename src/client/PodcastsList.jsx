f946e2d6df9df60f719508135d425f25ad05326f
tzoitzeot <tristan.maciag@icloud.com>
2020-07-01 13:51:00 +0200

    map podcasts & get axios

--- src/client/PodcastsList.jsx
+++ src/client/PodcastsList.jsx
@@ -1,15 +1,32 @@
-import React from 'react';
-import PodcastCard from './PodcastCard';
+import React, { useState, useEffect } from 'react';
 import { Container, Grid } from '@material-ui/core';
+import axios from 'axios';
+import PodcastCard from './PodcastCard';
 
 const PodcastsList = props => {
+  // Get podcasts request
+  const [podcasts, setPodcasts] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get('/podcast').catch(function(error) {
+        console.log(error.toJSON());
+      });
+      setPodcasts(result.data);
+    };
+    fetchData();
+  }, []);
+
   return (
     <div>
       <Container maxWidth="lg">
         <Grid container spacing={5}>
-          <Grid item xs={12} md={6} lg={4}>
-            <PodcastCard />
-          </Grid>
+          {podcasts.map(podcast => {
+            return (
+              <Grid item xs={12} md={6} lg={4}>
+                <PodcastCard dataPodcasts={podcast} />
+              </Grid>
+            );
+          })}
         </Grid>
       </Container>
     </div>
