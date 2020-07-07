422da286c4ce42ecf4e83db830863bc26c78c273
TOBOTOMAS31 <thomas.couderc5@gmail.com>
2020-07-07 15:25:16 +0200

    login OK

--- src/client/LoginPage.jsx
+++ src/client/LoginPage.jsx
@@ -1,12 +1,14 @@
 /* eslint-disable no-console */
 /* eslint-disable no-undef */
 /* eslint-disable no-alert */
-import React from 'react';
+import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
+import { Redirect } from 'react-router-dom';
 import axios from 'axios';
 import { TextField, Box, Grid, Button } from '@material-ui/core';
 
 const LoginPage = () => {
+  const [redirection, setRedirection] = useState(false);
   const { handleSubmit, register } = useForm();
   const onSubmit = data => {
     const dataForms = {
@@ -15,10 +17,16 @@ const LoginPage = () => {
     axios
       .post('/login', dataForms)
       .then(res => res.data)
-      .then(res => console.log(res))
+      .then(res => {
+        if (res.message === 'OK') {
+          return setRedirection({
+            redirection: true
+          });
+        }
+      })
       .catch(e => {
         console.error(e);
-        alert(`Erreur authentification' ${e.message}`);
+        alert('Mauvais mot de passe ou mauvais email');
       });
   };
 
@@ -48,6 +56,7 @@ const LoginPage = () => {
           </Grid>
 
           <Grid item xs={12}>
+            {redirection && <Redirect to="/admin-radio-occitanie" />}
             <Button type="submit" variant="contained" color="primary">
               Se connecter
             </Button>
