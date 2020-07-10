9d64a5e0fcc9d2f925c8bf3c03b3a5e46ba9fe94
tzoitzeot <tristan.maciag@icloud.com>
2020-07-10 10:51:07 +0200

    delete material ui card

--- src/client/ProgramCard.jsx
+++ src/client/ProgramCard.jsx
@@ -6,20 +6,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
 import './ProgramCard.scss';
-
-const useStyles = makeStyles({
-  root: {
-    maxWidth: '100%'
-  }
-});
 
 const ProgramCard = props => {
   const {
@@ -64,7 +52,6 @@ const ProgramCard = props => {
     fetchData();
   }, []);
 
-  const classes = useStyles();
   const image = program_image || '/radio-occitanie-default.jpg';
 
   return (
