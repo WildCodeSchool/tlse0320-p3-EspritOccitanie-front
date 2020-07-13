94da8425f1e9462b5080c533b0c1ea99798ec958
tzoitzeot <tristan.maciag@icloud.com>
2020-07-13 15:29:30 +0200

    add mobile link

--- src/client/Navbar.jsx
+++ src/client/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
@@ -20,6 +20,9 @@ const useStyles = makeStyles(theme => ({
 
 export default function Navbar() {
   const classes = useStyles();
+  const [mobileNavActive, setMobileNavActive] = useState(false);
+
+  console.log(mobileNavActive);
 
   return (
     <div className="navBar">
@@ -47,11 +50,38 @@ export default function Navbar() {
               </Link>
             </span>
 
-            <div className="burger">
-              <img src="/burger.svg" alt="menu burger" />
+            <div
+              className="burger"
+              onClick={
+                mobileNavActive ? () => setMobileNavActive(false) : () => setMobileNavActive(true)
+              }
+            >
+              <img src={mobileNavActive ? '/close.svg' : '/burger.svg'} alt="menu burger" />
             </div>
           </Toolbar>
         </AppBar>
+      </div>
+
+      <div className={!mobileNavActive ? 'nav-mobile' : 'nav-mobile active'}>
+        <Link className="link" to="direct" onClick={() => setMobileNavActive(false)}>
+          <Button>Le direct</Button>
+        </Link>
+        <Link className="link" to="/qui-sommes-nous" onClick={() => setMobileNavActive(false)}>
+          <Button>Qui sommes nous ?</Button>
+        </Link>
+        <Link
+          className="link"
+          to="/grille-des-programmes"
+          onClick={() => setMobileNavActive(false)}
+        >
+          <Button>Grille des programmes</Button>
+        </Link>
+        <Link className="link" to="/emissions" onClick={() => setMobileNavActive(false)}>
+          <Button>Émissions</Button>
+        </Link>
+        <Link className="link" to="/podcasts" onClick={() => setMobileNavActive(false)}>
+          <Button>Podcasts</Button>
+        </Link>
       </div>
     </div>
   );
