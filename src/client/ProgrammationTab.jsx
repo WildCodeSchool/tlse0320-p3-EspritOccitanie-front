3063d2f4c0e5c3add690f0335154a681268d9999
TOBOTOMAS31 <thomas.couderc5@gmail.com>
2020-07-15 20:35:13 +0200

    ajout bouton pour ajouter calendrier au calendrier client

--- src/client/ProgrammationTab.jsx
+++ src/client/ProgrammationTab.jsx
@@ -8,8 +8,12 @@ import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
+import AddCircleOutlineSharpIcon from '@material-ui/icons/AddCircleOutlineSharp';
+import Tooltip from '@material-ui/core/Tooltip';
 import ProgrammationCard from './ProgrammationCard';
 import './Programmation.scss';
+import './Apropos.scss';
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -78,134 +82,144 @@ export default function ScrollableTabsButtonAuto() {
   }, []);
 
   return (
-    <div className={classes.root}>
-      <AppBar position="static" color="default">
-        <Tabs
-          value={value}
-          onChange={handleChange}
-          indicatorColor="primary"
-          textColor="primary"
-          variant="scrollable"
-          scrollButtons="auto"
-          aria-label="scrollable auto tabs example"
-        >
-          {programmation.map((program, i) => {
-            return <Tab label={program.date} {...a11yProps(i)} />;
-          })}
-        </Tabs>
-      </AppBar>
-      <TabPanel value={value} index={0}>
-        <Container maxWidth="lg">
-          <Grid container spacing={2}>
-            {programmation.length > 0 &&
-              programmation[0].emissions.map(program => {
-                console.log(programmation.length);
-                console.log(program);
-                return (
-                  <Grid item xs={12} sm={6} md={4} lg={4}>
-                    <ProgrammationCard program={program} />
-                  </Grid>
-                );
-              })}
-          </Grid>
-        </Container>
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        <Container maxWidth="lg">
-          <Grid container spacing={2}>
-            {programmation.length > 0 &&
-              programmation[1].emissions.map(program => {
-                console.log(programmation.length);
-                console.log(program);
-                return (
-                  <Grid item xs={12} sm={6} md={4} lg={4}>
-                    <ProgrammationCard program={program} />
-                  </Grid>
-                );
-              })}
-          </Grid>
-        </Container>
-      </TabPanel>
-      <TabPanel value={value} index={2}>
-        <Container maxWidth="lg">
-          <Grid container spacing={2}>
-            {programmation.length > 0 &&
-              programmation[2].emissions.map(program => {
-                console.log(programmation.length);
-                console.log(program);
-                return (
-                  <Grid item xs={12} sm={6} md={4} lg={4}>
-                    <ProgrammationCard program={program} />
-                  </Grid>
-                );
-              })}
-          </Grid>
-        </Container>
-      </TabPanel>
-      <TabPanel value={value} index={3}>
-        <Container maxWidth="lg">
-          <Grid container spacing={2}>
-            {programmation.length > 0 &&
-              programmation[3].emissions.map(program => {
-                console.log(programmation.length);
-                console.log(program);
-                return (
-                  <Grid item xs={12} sm={6} md={4} lg={4}>
-                    <ProgrammationCard program={program} />
-                  </Grid>
-                );
-              })}
-          </Grid>
-        </Container>
-      </TabPanel>
-      <TabPanel value={value} index={4}>
-        <Container maxWidth="lg">
-          <Grid container spacing={2}>
-            {programmation.length > 0 &&
-              programmation[4].emissions.map(program => {
-                console.log(programmation.length);
-                console.log(program);
-                return (
-                  <Grid item xs={12} sm={6} md={4} lg={4}>
-                    <ProgrammationCard program={program} />
-                  </Grid>
-                );
-              })}
-          </Grid>
-        </Container>
-      </TabPanel>
-      <TabPanel value={value} index={5}>
-        <Container maxWidth="lg">
-          <Grid container spacing={2}>
-            {programmation.length > 0 &&
-              programmation[5].emissions.map(program => {
-                console.log(programmation.length);
-                console.log(program);
-                return (
-                  <Grid item xs={12} sm={6} md={4} lg={4}>
-                    <ProgrammationCard program={program} />
-                  </Grid>
-                );
-              })}
-          </Grid>
-        </Container>
-      </TabPanel>
-      <TabPanel value={value} index={6}>
-        <Container maxWidth="lg">
-          <Grid container spacing={2}>
-            {programmation.length > 0 &&
-              programmation[6].emissions.map(program => {
-                console.log(programmation.length);
-                console.log(program);
-                return (
-                  <Grid item xs={12} sm={6} md={4} lg={4}>
-                    <ProgrammationCard program={program} />
-                  </Grid>
-                );
-              })}
-          </Grid>
-        </Container>
-      </TabPanel>
+    <div className="wrapper-about">
+      <section className="full-blue header-title">
+        <h1>Programmation</h1>
+      </section>
+      <div className={classes.root}>
+        <AppBar position="static" color="default">
+          <Tabs
+            value={value}
+            onChange={handleChange}
+            indicatorColor="primary"
+            textColor="primary"
+            variant="scrollable"
+            scrollButtons="auto"
+            aria-label="scrollable auto tabs example"
+          >
+            {programmation.map((program, i) => {
+              return <Tab label={program.date} {...a11yProps(i)} />;
+            })}
+            <Button href="https://calendar.google.com/calendar?cid=NGRvZnNuOGc5cjUzb2xxYnVkcnEwcTAxMjhAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ">
+              <Tooltip title="Ajouter à mon calendrier Google" aria-label="Add">
+                <AddCircleOutlineSharpIcon fontSize="medium" />
+              </Tooltip>
+            </Button>
+          </Tabs>
+        </AppBar>
+        <TabPanel value={value} index={0}>
+          <Container maxWidth="lg">
+            <Grid container spacing={2}>
+              {programmation.length > 0 &&
+                programmation[0].emissions.map(program => {
+                  console.log(programmation.length);
+                  console.log(program);
+                  return (
+                    <Grid item xs={12} sm={6} md={4} lg={4}>
+                      <ProgrammationCard program={program} />
+                    </Grid>
+                  );
+                })}
+            </Grid>
+          </Container>
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+          <Container maxWidth="lg">
+            <Grid container spacing={2}>
+              {programmation.length > 0 &&
+                programmation[1].emissions.map(program => {
+                  console.log(programmation.length);
+                  console.log(program);
+                  return (
+                    <Grid item xs={12} sm={6} md={4} lg={4}>
+                      <ProgrammationCard program={program} />
+                    </Grid>
+                  );
+                })}
+            </Grid>
+          </Container>
+        </TabPanel>
+        <TabPanel value={value} index={2}>
+          <Container maxWidth="lg">
+            <Grid container spacing={2}>
+              {programmation.length > 0 &&
+                programmation[2].emissions.map(program => {
+                  console.log(programmation.length);
+                  console.log(program);
+                  return (
+                    <Grid item xs={12} sm={6} md={4} lg={4}>
+                      <ProgrammationCard program={program} />
+                    </Grid>
+                  );
+                })}
+            </Grid>
+          </Container>
+        </TabPanel>
+        <TabPanel value={value} index={3}>
+          <Container maxWidth="lg">
+            <Grid container spacing={2}>
+              {programmation.length > 0 &&
+                programmation[3].emissions.map(program => {
+                  console.log(programmation.length);
+                  console.log(program);
+                  return (
+                    <Grid item xs={12} sm={6} md={4} lg={4}>
+                      <ProgrammationCard program={program} />
+                    </Grid>
+                  );
+                })}
+            </Grid>
+          </Container>
+        </TabPanel>
+        <TabPanel value={value} index={4}>
+          <Container maxWidth="lg">
+            <Grid container spacing={2}>
+              {programmation.length > 0 &&
+                programmation[4].emissions.map(program => {
+                  console.log(programmation.length);
+                  console.log(program);
+                  return (
+                    <Grid item xs={12} sm={6} md={4} lg={4}>
+                      <ProgrammationCard program={program} />
+                    </Grid>
+                  );
+                })}
+            </Grid>
+          </Container>
+        </TabPanel>
+        <TabPanel value={value} index={5}>
+          <Container maxWidth="lg">
+            <Grid container spacing={2}>
+              {programmation.length > 0 &&
+                programmation[5].emissions.map(program => {
+                  console.log(programmation.length);
+                  console.log(program);
+                  return (
+                    <Grid item xs={12} sm={6} md={4} lg={4}>
+                      <ProgrammationCard program={program} />
+                    </Grid>
+                  );
+                })}
+            </Grid>
+          </Container>
+        </TabPanel>
+        <TabPanel value={value} index={6}>
+          <Container maxWidth="lg">
+            <Grid container spacing={2}>
+              {programmation.length > 0 &&
+                programmation[6].emissions.map(program => {
+                  console.log(programmation.length);
+                  console.log(program);
+                  return (
+                    <Grid item xs={12} sm={6} md={4} lg={4}>
+                      <ProgrammationCard program={program} />
+                    </Grid>
+                  );
+                })}
+            </Grid>
+          </Container>
+        </TabPanel>
+      </div>
     </div>
   );
 }
