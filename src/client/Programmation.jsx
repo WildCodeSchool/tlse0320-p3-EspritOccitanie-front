b2061d62118833d020bb317d188c582a0a505143
TOBOTOMAS31 <thomas.couderc5@gmail.com>
2020-07-15 17:48:02 +0200

    commit before pr

--- src/client/Programmation.jsx
+++ src/client/Programmation.jsx
@@ -1,49 +1,49 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-// import moment from 'moment';
-import Container from '@material-ui/core/Container';
-import Grid from '@material-ui/core/Grid';
-import ProgrammationCard from './ProgrammationCard';
+// import React, { useEffect, useState } from 'react';
+// import axios from 'axios';
+// // import moment from 'moment';
+// import Container from '@material-ui/core/Container';
+// import Grid from '@material-ui/core/Grid';
+// import ProgrammationCard from './ProgrammationCard';
 
-const Programmation = () => {
-  const [programmation, setProgrammation] = useState([]);
+// const Programmation = () => {
+//   const [programmation, setProgrammation] = useState([]);
 
-  // get programmation
-  useEffect(() => {
-    axios
-      .get(
-        'https://script.google.com/macros/s/AKfycbya2CcsOpKGTpl3rC6c4bGk-JfKNnKgcbB6fMxwi53NH-_wdms/exec'
-      )
-      .then(res => {
-        return setProgrammation(res.data);
-      })
-      .catch(e => console.error(e));
-  }, []);
+//   // get programmation
+//   useEffect(() => {
+//     axios
+//       .get(
+//         'https://script.google.com/macros/s/AKfycbya2CcsOpKGTpl3rC6c4bGk-JfKNnKgcbB6fMxwi53NH-_wdms/exec'
+//       )
+//       .then(res => {
+//         return setProgrammation(res.data);
+//       })
+//       .catch(e => console.error(e));
+//   }, []);
 
-  return (
-    <div>
-      <Container maxWidth="lg">
-        {programmation.map(program => {
-          return (
-            <div>
-              <Grid container spacing={3}>
-                <Grid item xs={12}>
-                  <p>{program.date}</p>
-                  <ProgrammationCard
-                    program={program.emissions}
-                    setProgrammation={setProgrammation}
-                  />
-                </Grid>
-              </Grid>
-            </div>
-          );
-        })}
-      </Container>
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <Container maxWidth="lg">
+//         {programmation.map(program => {
+//           return (
+//             <div>
+//               <Grid container spacing={3}>
+//                 <Grid item xs={12}>
+//                   <p>{program.date}</p>
+//                   <ProgrammationCard
+//                     program={program.emissions}
+//                     setProgrammation={setProgrammation}
+//                   />
+//                 </Grid>
+//               </Grid>
+//             </div>
+//           );
+//         })}
+//       </Container>
+//     </div>
+//   );
+// };
 
-export default Programmation;
+// export default Programmation;
 
 // {program.emissions.map(emission => {
 //   return (
