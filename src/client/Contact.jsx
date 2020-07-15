9bd0e6772e0f88cd3a6ba348f9fa3c671adfa912
TOBOTOMAS31 <thomas.couderc5@gmail.com>
2020-07-15 11:38:13 +0200

    fix scss formulaire contact

--- src/client/Contact.jsx
+++ src/client/Contact.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
 import axios from 'axios';
-
+import './Contact.scss';
 import { TextField, Box, Grid, Button } from '@material-ui/core';
 
 const Contact = () => {
