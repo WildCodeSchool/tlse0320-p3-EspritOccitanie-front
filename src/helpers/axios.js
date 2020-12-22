617734d6713dbd86c22c459e8cf109fdf8717a9b
Geoffroy Periot <geoffroy@wildcodeschool.fr>
2020-12-22 12:12:30 +0100

    Fix/ssr index redirection (#77) (#78)
    * Server setup ok

--- src/helpers/axios.js
+++ src/helpers/axios.js
@@ -1,10 +1,9 @@
 const axios = require('axios');
+const { SERVER_URL } = require('../env');
 
 const axiosInstance = axios.create({
   baseURL: SERVER_URL,
   timeout: 5000
 });
-
-console.log(axiosInstance.baseURL);
 
 module.exports = axiosInstance;
